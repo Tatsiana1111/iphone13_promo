41e658dbbcbc75b2ac1aafbd5db4640fc3d91b5c
Tatsiana1111 <tanyashket@gmail.com>
2022-01-23 11:38:27 +0300

    Initial commit

---
+++ scripts/scroll.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll(`.header-menu__item a`)
+
+seamless.polyfill()
+
+links.forEach((element) => {
+    element.addEventListener(`click`, () => {
+        console.log(`click`)
+    })
+    element.addEventListener(`click`, (event) => {
+        event.preventDefault()
+        const id = element.getAttribute(`href`).substring(1)
+        const section = document.getElementById(id)
+        if (section) {
+            seamless.elementScrollIntoView(section, {
+                behavior: `smooth`,
+                block: `start`
+            })
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#card"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
