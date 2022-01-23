1a10bd7ad2eb5a0d0e6fca7b03b9973a3ee0c4cb
Tatsiana1111 <tanyashket@gmail.com>
2022-01-23 13:02:39 +0300

    less2

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,29 +1,33 @@
-const links = document.querySelectorAll(`.header-menu__item a`)
-const linksCharacteristic = document.querySelector(`.card-details__link-characteristics`)
+const scrollFunc = () => {
+    const links = document.querySelectorAll(`.header-menu__item a`)
+    const linksCharacteristic = document.querySelector(`.card-details__link-characteristics`)
 
-const array = [...links, linksCharacteristic]
+    const array = [...links, linksCharacteristic]
 
-seamless.polyfill()
+    seamless.polyfill()
 
-array.forEach((element) => {
-    element.addEventListener(`click`, () => {
-        console.log(`click`)
+    array.forEach((element) => {
+        element.addEventListener(`click`, () => {
+            console.log(`click`)
+        })
+        element.addEventListener(`click`, (event) => {
+            event.preventDefault()
+            const id = element.getAttribute(`href`).substring(1)
+            const section = document.getElementById(id)
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: `smooth`,
+                    block: `start`
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#card"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
+        })
     })
-    element.addEventListener(`click`, (event) => {
-        event.preventDefault()
-        const id = element.getAttribute(`href`).substring(1)
-        const section = document.getElementById(id)
-        if (section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: `smooth`,
-                block: `start`
-            })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#card"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
-    })
-})
+}
+
+scrollFunc()
